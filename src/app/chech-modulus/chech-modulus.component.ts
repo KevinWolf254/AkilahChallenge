0b028f8dbffba8c6ea83ce73c1a263d5ca1bf288
Kanyi-PC <mbugua.kevinkanyi@gmail.com>
2018-04-05 14:03:24 +0300

    origin

--- src/app/chech-modulus/chech-modulus.component.ts
+++ src/app/chech-modulus/chech-modulus.component.ts
@@ -13,17 +13,17 @@ export class ChechModulusComponent implements OnInit {
 
   ngOnInit() {
   }
-print(){
-  let i: number;
-  let numStart: number = -50;
-  let numEnd: number = 50;
-  for (i = numStart; i<= numEnd; i++){
-    if(i % 3 == 0)
-      console.log(i+"Bash");
-    else if(i % 5 == 0)
-      console.log(i+"Bish");
-    if(i % 3 == 0 && i % 5 == 0)
-      console.log(i+"BishBash");
+  print(){
+    let i: number;
+    let numStart: number = -50;
+    let numEnd: number = 50;
+    for (i = numStart; i<= numEnd; i++){
+      if(i % 3 == 0)
+        console.log(i+"Bash");
+      else if(i % 5 == 0)
+        console.log(i+"Bish");
+      if(i % 3 == 0 && i % 5 == 0)
+        console.log(i+"BishBash");
+    }
   }
-}
 }
